691925a7c2931fce8bf9b9094e0feb6f52386a9b
gdotwill <godwillmark@yahoo.co.uk>
2025-02-03 21:38:32 +0200

    update

--- src/routes/users.js
+++ src/routes/users.js
@@ -7,7 +7,7 @@ import {
 const router = express.Router();
 
 // Define routes for various HTTP methods and their corresponding functions
-router.get("/", getUsers); // Get all users
+router.get("/", getUsers); 
 
 
 export default router;
